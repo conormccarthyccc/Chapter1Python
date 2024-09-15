c82c85501dd16bd58fa1672f8ee800b2810618af
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-09-15 12:38:34 +0100

    Add files via upload

---
+++ atm design.py
@@ -0,0 +1,23 @@
+# Date: Sept 2024
+# Author: Ms Hayes
+# Purpose: This code displays an ATM menu
+print("\t|-------------------------------|")
+print("\t|\t BANK OF IRELAND\t|")
+print("\t|\t ATM Main Menu\t\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t1. Current Balance\t|")
+print("\t|\t2. Cash Lodgement\t|")
+print("\t|\t3. Cash Withdrawal\t|")
+print("\t|\t4. Cash Transfer\t|")
+print("\t|\t5. Payment Logs\t\t|")
+print("\t|\t6. Cards\t\t|")
+print("\t|\t7. Change PIN\t\t|")
+print("\t|\t8. Other Services\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t Settings\t\t|")
+print("\t|\t Exit\t\t\t|")
+print("\t|-------------------------------|")
+print("\t|\t\t\t\t|")
+print("\t| CHOOSE AN OPTION >> \t\t|")
+print("\t|\t\t\t\t|")
+print("\t|-------------------------------|")
