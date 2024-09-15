c64a5f6233ccb2858bd800241656ba9e2c0300ea
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-09-15 12:58:04 +0100

    Add files via upload

---
+++ spotify menu design.py
@@ -0,0 +1,23 @@
+# Date: Sept 2024
+# Author: Ms Hayes
+# Purpose: This code displays an ATM menu
+print("\t|-------------------------------|")
+print("\t|\t Spotify\t\t|")
+print("\t|\t Main Menu\t\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t1. Music\t\t|")
+print("\t|\t2. Podcasts\t\t|")
+print("\t|\t3. Audiobooks\t\t|")
+print("\t|\t4. Playlists\t\t|")
+print("\t|\t5. Libary\t\t|")
+print("\t|\t6. Recently played\t|")
+print("\t|\t7. Recommended for you\t|")
+print("\t|\t8. Your Subscription\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t Settings\t\t|")
+print("\t|\t Exit\t\t\t|")
+print("\t|-------------------------------|")
+print("\t|\t\t\t\t|")
+print("\t| CHOOSE AN OPTION >> \t\t|")
+print("\t|\t\t\t\t|")
+print("\t|-------------------------------|")
