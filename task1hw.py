811615e1ebc88a47207fb241343713b50690eb34
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-09-19 17:05:26 +0100

    Add files via upload

---
+++ task1hw.py
@@ -0,0 +1,15 @@
+#Author: CMC
+# Date: Sept 24
+#Title: Favoutite artist program
+
+# input their favorite artist
+favorite_artist = input("Please enter the name of your favorite artist: ")
+
+# Create a compliment
+compliment = " is brilliant!"
+
+# Concatenate the two strings
+result = favorite_artist + compliment
+
+# Display the result
+print(result)
