62807fca9a8ff7f533b2842824e5ec418266a1f9
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-09-24 14:16:18 +0100

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,60 @@
+# Date: Sept 24
+# Author: CMC
+#Title: Learning turtle graphics
+
+import turtle
+# Square
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# 
+# turtle.exitonclick()
+
+# Octagon
+# for i in range(0,8):
+#     turtle.forward(120)
+#     turtle.right(45)
+# 
+# turtle.exitonclick()
+
+# Envelope
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+#     
+# for i in range(0,1):
+#     turtle.right(45)
+#     turtle.forward(75)
+#     turtle.left(90)
+#     turtle.forward(75)
+# 
+# turtle.exitonclick()
+
+# Star
+
+# for i in range (0,5):
+#      turtle.forward(100)
+#      turtle.right(144)
+
+#turtle.exitonclick()
+    
+# Pattern
+
+# for i in range(0,8):
+#     turtle.forward(120)
+#     turtle.right(45)
+    
+# for i in range(0,111):
+#     for i in range(0,8):
+#         turtle.forward(120)
+#         turtle.right(45)
+#     turtle.right(36)
+#      
+# 
+# turtle.exitonclick()
+
+# Background
+
+scr=turtle.Screen()
+scr.bgcolor("pink")
+# turtle.exitonclick()
