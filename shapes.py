2661e01a65b667d8a27a642e27e8c639edc1390f
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-09-24 19:40:16 +0100

    Add files via upload

--- shapes.py
+++ shapes.py
@@ -1,60 +1,31 @@
-# Date: Sept 24
 # Author: CMC
-#Title: Learning turtle graphics
+# Date: Sept 24
+# Title: Turtle
 
-import turtle
-# Square
-# for i in range(0,4):
-#     turtle.forward(100)
-#     turtle.right(90)
+# import turtle
 # 
-# turtle.exitonclick()
-
-# Octagon
-# for i in range(0,8):
-#     turtle.forward(120)
-#     turtle.right(45)
-# 
-# turtle.exitonclick()
-
-# Envelope
 # for i in range(0,4):
 #     turtle.forward(100)
 #     turtle.right(90)
 #     
-# for i in range(0,1):
-#     turtle.right(45)
-#     turtle.forward(75)
-#     turtle.left(90)
-#     turtle.forward(75)
-# 
 # turtle.exitonclick()
 
-# Star
+# Octagon
 
-# for i in range (0,5):
-#      turtle.forward(100)
-#      turtle.right(144)
-
-#turtle.exitonclick()
-    
-# Pattern
-
+# import turtle
+# 
 # for i in range(0,8):
 #     turtle.forward(120)
 #     turtle.right(45)
+#     
+# turtle.exitonclick()
+
+# Envelope
+
+import turtle
+
+for i in range(0,4):
+    turtle.forward(120)
+    turtle.right(45)
     
-# for i in range(0,111):
-#     for i in range(0,8):
-#         turtle.forward(120)
-#         turtle.right(45)
-#     turtle.right(36)
-#      
-# 
-# turtle.exitonclick()
-
-# Background
-
-scr=turtle.Screen()
-scr.bgcolor("pink")
-# turtle.exitonclick()
+turtle.exitonclick()
