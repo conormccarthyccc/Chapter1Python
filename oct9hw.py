737011110d536d1961aff9bedc2ce09ee1ff312d
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-10-09 19:41:10 +0100

    Add files via upload

---
+++ oct9hw.py
@@ -0,0 +1,12 @@
+  # Simple while loop
+
+  # Initialise a loop counter
+  counter = 1
+  
+  # Loop 10 times
+  while counter <= 10:
+      print("Hello World") # Display a message
+      counter = counter + 1 # Increment the counter
+      
+  # This line is only executed once
+  print("Goodbye")
