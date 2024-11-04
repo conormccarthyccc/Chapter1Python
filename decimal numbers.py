209f7cfaf92e492b037ffe05ba711737d8ba2480
CMC <ctw20.conor.mccarthy@corketb.ie>
2024-11-04 18:35:44 +0000

    Add files via upload

---
+++ decimal numbers.py
@@ -0,0 +1,9 @@
+# Title: Homework 4/11
+# Author: CMC
+
+decimal_number = 301
+
+hexadecimal_string = hex(decimal_number)[2:]
+
+# Result
+print(f"The hexadecimal representation of {decimal_number} is: {hexadecimal_string.upper()}")
